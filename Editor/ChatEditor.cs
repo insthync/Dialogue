fc7bcb45a3cfafa8e558acba0808b3fc5c86bbcf
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-07-12 03:33:55 +0700

    Add title

--- Editor/ChatEditor.cs
+++ Editor/ChatEditor.cs
@@ -29,7 +29,10 @@ namespace Dialogue
             }
             GUILayout.Space(-30);
 
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("title"), GUIContent.none);
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("titleByLanguageKeys"), GUIContent.none);
             NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("text"), GUIContent.none);
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("textByLanguageKeys"), GUIContent.none);
             NodeEditorGUILayout.DynamicPortList("answers", typeof(DialogueBaseNode), serializedObject, NodePort.IO.Output, Node.ConnectionType.Override);
 
             serializedObject.ApplyModifiedProperties();
