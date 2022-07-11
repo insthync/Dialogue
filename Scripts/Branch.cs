8b5355dcc255258102dc6a5397935c1587eba97c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-07-11 23:13:18 +0700

    Delete unused namespace

--- Scripts/Branch.cs
+++ Scripts/Branch.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using UnityEngine;
-using UnityEngine.Events;
 using XNode;
 
 namespace Dialogue
