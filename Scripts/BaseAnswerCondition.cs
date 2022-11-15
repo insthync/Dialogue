a1d143846ce9d1df74d595a1c7f2e841064ec243
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-11-15 08:09:14 +0700

    Change is pass function to be async

--- Scripts/BaseAnswerCondition.cs
+++ Scripts/BaseAnswerCondition.cs
@@ -1,9 +1,10 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Dialogue
 {
     public abstract class BaseAnswerCondition : ScriptableObject
     {
-        public abstract bool IsPass();
+        public abstract Task<bool> IsPass();
     }
 }
